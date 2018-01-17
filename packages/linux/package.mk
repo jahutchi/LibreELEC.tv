a5998a65d3d290c472ecc1815eb5495235e0135f
jahutchi <jahutchinson99@googlemail.com>
2018-01-17 13:55:04 +0000

    Switched to kernel v4.9.76

--- packages/linux/package.mk
+++ packages/linux/package.mk
@@ -64,7 +64,7 @@ case "$LINUX" in
     PKG_PATCH_DIRS="default-rpi"
     ;;
   *)
-    PKG_VERSION="4.11.12"
+    PKG_VERSION="4.9.76"
     PKG_URL="http://www.kernel.org/pub/linux/kernel/v4.x/$PKG_NAME-$PKG_VERSION.tar.xz"
     PKG_PATCH_DIRS="default"
     ;;

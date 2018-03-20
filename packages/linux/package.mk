8bde2b942b61f6b51d29168b16e4a1938ca8d27c
jahutchi <jahutchinson99@googlemail.com>
2018-03-20 12:57:11 +0000

    Upgraded kernel (generic) to 4.9.88

--- packages/linux/package.mk
+++ packages/linux/package.mk
@@ -64,7 +64,7 @@ case "$LINUX" in
     PKG_PATCH_DIRS="default-rpi"
     ;;
   *)
-    PKG_VERSION="4.9.86"
+    PKG_VERSION="4.9.88"
     PKG_URL="http://www.kernel.org/pub/linux/kernel/v4.x/$PKG_NAME-$PKG_VERSION.tar.xz"
     PKG_PATCH_DIRS="default"
     ;;
